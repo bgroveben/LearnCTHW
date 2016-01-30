89072ba2fbd403c824a9ad4001e288647f19c963
bgrove1974 <bgroveben@gmail.com>
2016-01-30 01:44:55 -0800

    Start Exercise 1

--- ex1.c
+++ ex1.c
@@ -1,6 +1,18 @@
+/*
+Exercise 1: Dust Off That Compiler
+Here is a simple first program you can make in C:
+*/
 int main(int argc, char *argv[])
 {
     puts("Hello world.");
 
     return 0;
 }
+/*
+Next, go to the terminal and type:
+$ make ex1
+Then type:
+$ ./ex1
+The output should be:
+Hello world.
+*/
