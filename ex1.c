f392f8910bdef320f0d86e7bbe4236741c03138a
bgrove1974 <bgroveben@gmail.com>
2016-01-30 23:34:58 -0800

    Finish exercise 1

--- ex1.c
+++ ex1.c
@@ -2,9 +2,11 @@
 Exercise 1: Dust Off That Compiler
 Here is a simple first program you can make in C:
 */
+#include <stdio.h> /* This line gets rid of compiler warnings */
 int main(int argc, char *argv[])
 {
     puts("Hello world.");
+    puts("This is the second line.");
 
     return 0;
 }
