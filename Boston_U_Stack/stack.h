cf8569f680ffa72d488f6cd8dfffdc1f1a2e9131
bgroveben <bgroveben@gmail.com>
2016-06-16 03:28:36 -0700

    Create a struct for the stack array implementation

--- Boston_U_Stack/stack.h
+++ Boston_U_Stack/stack.h
@@ -1,10 +1,35 @@
 // Prototypes for each stack function go here
 
-typedef char stackElementT;  /* Give it a generic name--makes  */
-                             /* changing the type of things in */
-                             /* the stack easy.                */
+/*
+File: stack.h
+Author: Robert I. Pitts <rip@cs.bu.edu>
+Last Modified: June 16, 2016 by Benjamin Grove
+Topic: Stack - Array Implementation
+
+This is the interface for a stack of characters
+*/
+
+#ifndef _STACK_H
+#define _STACK_H
+
+/*
+Type: stackElementT
+This is the type of the objects entered in the stack.
+Edit it to change the type of things to be placed in the stack.
+*/
+
+typedef char stackElementT;
+
+/*
+Type: stackElementT
+This is the type for a stack, i.e., it is a type that holds the information necessary to keep track of a stack.
+It has a pointer 'contents' to a dynamically-allocated array (used to hold the contents of the stack),
+an integer 'maxSize' that holds the size of this array (i.e., the maximum number of things that can be held in the stack),
+and another integer 'top', which stores the array index of the element at the top of the stack.
+*/
+
 typedef struct {
-    stackElementT *contents  // contents is a pointer since it will be dynamically allocated
+    stackElementT *contents;
+    int maxSize;
     int top;
-    int maxSize;  // maximum size of the array is determined at run-time (not compile time) 
-} stackT;
+}   stackT;
