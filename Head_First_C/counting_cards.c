c874886292476766e0c2108644c3572b3167adc3
bgroveben <bgroveben@gmail.com>
2017-11-02 16:34:18 -0700

    Intro to counting cards using C

--- Head_First_C/counting_cards.c
+++ Head_First_C/counting_cards.c
@@ -1,12 +1,16 @@
 /*
- * Program to evaluate face values.
- * Released under the Yo Momma's House Public License.
- * (c) 2017 Ben's Blackjack Team
- */
-
-/*
- * To compile and run this code from the command line:
- * => gcc cards.c -o cards && ./cards
+ * Program to count cards in a blackjack game.
+ * https://en.wikipedia.org/wiki/Card_counting
+ *
+ * Released under the DGAF Public License.
+ * (c)2017 Ben's Blackjack Team.
+ * 
+ * The recipe:
+ * Evaluate the card.
+ * Is it between 3 and 6 (inclusive)?
+ *     Increase count by 1.
+ * Otherwise...
+ *     Decrease count by 1.
  */
 
 #include <stdio.h>
@@ -15,7 +19,7 @@
 int main()
 {
 	char card_name[3];
-	puts("Enter the card_name: ");
+	puts("Enter the card name: ");
 	scanf("%2s", card_name);
 	int val = 0;
 	if (card_name[0] == 'K') {
--- README.md
+++ README.md
@@ -33,4 +33,4 @@ Printed in the United States of America.
 Published by O’Reilly Media, Inc., 1005 Gravenstein Highway North, Sebastopol, CA 95472.  
 ISBN: 978-1-449-39991-7  
 ```
-I am on Chapter 1, pp 50, Test Drive.
+I am on Chapter 1, pp 57, Card Counting.
