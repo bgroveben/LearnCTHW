c06a0e92de16fa7d8c82b83045b9ecce349dc59b
bgroveben <bgroveben@gmail.com>
2017-11-02 17:14:01 -0700

    Booleans in C

---
+++ Head_First_C/booleans.c
@@ -0,0 +1,16 @@
+/*
+* There's more to booleans than equals...
+* This code will not compile or run without a main method.
+*/
+
+/* && checks if two conditions are true. */
+if ((dealer_up_card == 6) && (hand == 11))
+	double_down();
+
+/* || checks if one (and only one) of the conditions is true. */
+if (cupcakes_in_fridge || chips_on_table)
+	eat_food();
+
+/* ! flips the value of a condition. */
+if (!brad_on_phone)
+	answer_phone();
