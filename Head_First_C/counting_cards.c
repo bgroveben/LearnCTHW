4ebdde4ce55d4ee3d0c7936185ed9e9b4b6d3797
bgroveben <bgroveben@gmail.com>
2017-11-02 20:42:20 -0700

    Use switch statement to assign values to their corresponding cards

--- Head_First_C/counting_cards.c
+++ Head_First_C/counting_cards.c
@@ -21,18 +21,21 @@ int main()
 	char card_name[3];
 	puts("Enter the card name: ");
 	scanf("%2s", card_name);
+	/* Assign a value to each card: */
 	int val = 0;
-	if (card_name[0] == 'K') {
+	switch(card_name[0]){
+	case 'K':
+	case 'Q'):
+	case 'J'):
 		val = 10;
-	} else if (card_name[0] == 'Q') {
-		val = 10;
-	} else if (card_name[0] == 'J') {
-		val = 10;
-	} else if (card_name[0] == 'A') {
+		break;
+	case 'A':
 		val = 11;
-	} else {
+		break;
+	default:
 		val = atoi(card_name);
 	}
+
 	/* Check if the value is 3 to 6: */
 	if ((val > 2) && (val < 7))
 		puts("Count has gone up.");
