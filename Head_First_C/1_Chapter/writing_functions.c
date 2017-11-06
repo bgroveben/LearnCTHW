30aa343bbaab73b4c6cf8c5fdb6223f5178f4088
bgroveben <bgroveben@gmail.com>
2017-11-06 02:05:10 -0800

    Writing functions in C

---
+++ Head_First_C/1_Chapter/writing_functions.c
@@ -0,0 +1,8 @@
+/* The main() function: */
+
+int main()
+{ // The body of the function is surrounded by braces:
+	puts("I'm too sexy for my code.");
+	/* When you're done, return a value: */
+	return 0;
+}
