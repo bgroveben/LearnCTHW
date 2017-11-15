9852fe44d450aa328ab0900e07496615ba9fb051
bgroveben <bgroveben@gmail.com>
2017-11-15 12:00:08 -0800

    Modify card program to keep a running count of the card game and allow player to quit by typing 'X'

--- Head_First_C/1_Chapter/cards.c
+++ Head_First_C/1_Chapter/cards.c
@@ -15,20 +15,39 @@
 int main()
 {
 	char card_name[3];
-	puts("Enter the card_name: ");
-	scanf("%2s", card_name);
-	int val = 0;
-	if (card_name[0] == 'K') {
-		val = 10;
-	} else if (card_name[0] == 'Q') {
-		val = 10;
-	} else if (card_name[0] == 'J') {
-		val = 10;
-	} else if (card_name[0] == 'A') {
-		val = 11;
-	} else {
-		val = atoi(card_name);
-	}
-	printf("The card value is: %i\n", val);
-	return 0;
+    int count = 0;
+    while(card_name[0] != 'X')
+    {
+    	puts("Enter the card_name: ");
+    	scanf("%2s", card_name);
+    	int val = 0;
+    	switch(card_name[0]) {
+            case 'K':
+            case 'Q':
+            case 'J':
+                val = 10;
+                break;
+            case 'A':
+                val = 11;
+                break;
+            case 'X':
+                // We're inside a switch statement,
+                // so use continue instead of break.
+                continue;
+            default:
+                val = atoi(card_name);
+                if ((val < 1) || (val > 10)) {
+                    puts("Invalid value");
+                    continue;
+                }
+            }
+            if ((val > 2) && (val < 7)) {
+                count++;
+            } else if (val == 10) {
+                count--;
+            }
+            printf("The VALUE is: %i\n", val);
+    	    printf("The COUNT is: %i\n", count);
+    }
+    return 0;
 }
