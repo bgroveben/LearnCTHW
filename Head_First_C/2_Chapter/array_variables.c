2a37ec9935f101801a2a915322acf54ac90cede2
bgroveben <bgroveben@gmail.com>
2017-11-16 18:57:50 -0800

    What the computer thinks when it runs your code

---
+++ Head_First_C/2_Chapter/array_variables.c
@@ -0,0 +1,16 @@
+/*
+* When you create an array, the array variable can be used as a pointer to the
+* start of the array in memory.
+* The computer will set aside space on the stack for each of the characters in
+* the string, plus the \0 end character.
+* C will also associate the address of the first character with the name of the
+* variable.
+*/
+#include <stdio.h>
+
+int main()
+{
+    char quote[] = "Cookies make you sexy.";
+    printf("The quote string is stored at : %p\n", quote);
+    return 0;
+}
--- Head_First_C/2_Chapter/pass_string_to_function.c
+++ Head_First_C/2_Chapter/pass_string_to_function.c
@@ -7,16 +7,27 @@
 */
 
 #include <stdio.h>
+
+char quote[] = "C has an operator called sizeof that can tell you how many bytes of space something takes in memory. You can either call it with a data type or with a piece of data.";
 // The function will be passed a char array:
 void fortune_cookie(char msg[])
 {
     printf("Message reads: %s\n", msg);
-    printf("The variable named msg occupies %i bytes\n", sizeof(msg));
+    printf("The variable named msg occupies %lu bytes\n", sizeof(msg));
+    printf("The size of the string contained in the variable named quote: %lu bytes \n", sizeof(quote));
 }
 
 int main()
 {
-    char quote[] = "C has an operator called sizeof that can tell you how many bytes of space something takes in memory. You can either call it with a data type or with a piece of data.";
     fortune_cookie(quote);
     return 0;
 }
+/*
+* When this file is compiled, you will see a warning:
+*
+* warning: sizeof on array function parameter will return size of 'char *'
+* instead of 'char []'
+*
+* telling you that you are lookin up the size of the pointer instead of the
+* size of the string it is pointing to.
+*/
