f46973098d481e2e429dfc90f0a8f577e5533885
bgroveben <bgroveben@gmail.com>
2017-11-16 18:14:19 -0800

    Add main method to file about passing strings to functions

--- Head_First_C/2_Chapter/pass_string_to_function.c
+++ Head_First_C/2_Chapter/pass_string_to_function.c
@@ -6,12 +6,17 @@
 * That means if you want to pass a string to a function, you can do it like this:
 */
 
+#include <stdio.h>
 // The function will be passed a char array:
 void fortune_cookie(char msg[])
 {
     printf("Message reads: %s\n", msg);
-    printf("msg occupies %i bytes\n", sizeof(msg));
+    printf("The variable named msg occupies %i bytes\n", sizeof(msg));
 }
 
-char quote[] = "Cookies make you sexy";
-fortune_cookie(quote);
+int main()
+{
+    char quote[] = "C has an operator called sizeof that can tell you how many bytes of space something takes in memory. You can either call it with a data type or with a piece of data.";
+    fortune_cookie(quote);
+    return 0;
+}
