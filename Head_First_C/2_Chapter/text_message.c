18d08b9273e06a1ea5a79e16a27e9a22b255a737
bgroveben <bgroveben@gmail.com>
2017-11-19 03:34:52 -0800

    Experimenting with pointer arithmetic

---
+++ Head_First_C/2_Chapter/text_message.c
@@ -0,0 +1,23 @@
+/*
+* Use the power of pointer arithmetic to mend a broken heart.
+* This function will skip the first six characters of the text message.
+*/
+#include <stdio.h>
+
+int main()
+{
+    char *msg_from_amy = "Don't call me";
+    skip(msg_from_amy);
+    return 0;
+}
+int skip(char *msg)
+{
+    puts(msg + 6);
+    return 0;
+}
+/*
+* C Header files:
+* http://en.cppreference.com/w/c/header
+* The C Preprocessor:
+* https://gcc.gnu.org/onlinedocs/gcc-3.0.2/cpp.html#SEC_Top
+*/
--- README.md
+++ README.md
@@ -36,4 +36,5 @@ Printed in the United States of America.
 Published by O’Reilly Media, Inc., 1005 Gravenstein Highway North, Sebastopol, CA 95472.  
 ISBN: 978-1-449-39991-7  
 ```
-I am on Chapter 2, pp 101.
+I am on Chapter 2, pp 102.
+Why pointers have types.
