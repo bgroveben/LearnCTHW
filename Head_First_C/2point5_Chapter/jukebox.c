2deb84348403c4934bfc4e8415e5d3c997e6eb00
bgroveben <bgroveben@gmail.com>
2017-12-03 02:07:42 -0800

    Begin building a jukebox program

---
+++ Head_First_C/2point5_Chapter/jukebox.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char tracks[][80] = {
+    "I left my heart in Harvard Med School",
+    "Newark, Newark - a wonderful town",
+    "Dancing with a Dork",
+    "From here to maternity",
+    "The girl from Iwo Jima",
+};
+
+int main()
+{
+    /* code for whatever */
+    return 0;
+}
--- README.md
+++ README.md
@@ -39,5 +39,5 @@ Printed in the United States of America.
 Published by O’Reilly Media, Inc., 1005 Gravenstein Highway North, Sebastopol, CA 95472.  
 ISBN: 978-1-449-39991-7  
 ```
-I am on Chapter 2.5, pp 123.
-Strings
+I am on Chapter 2.5, pp 129.
+Using the `strstr()` function
