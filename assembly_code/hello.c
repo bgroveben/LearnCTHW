08b21319bfd81b57af22d781de5fc1fd39790f25
bgroveben <bgroveben@gmail.com>
2017-12-05 03:25:48 -0800

    Hello world in Assembly

---
+++ assembly_code/hello.c
@@ -0,0 +1,17 @@
+/*
+********************************************************************************
+* Introduction to X86-64 Assembly for Compiler Writers
+* by Prof. Douglas Thain
+* Copyright (C) 2015 The University of Notre Dame
+* This work is licensed under a Creative Commons Attribution-ShareAlike 4.0
+* International License.
+(Last updated 9-Nov-2015)
+*******************************************************************************
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello %s\n","World!");
+    return 0;
+}
